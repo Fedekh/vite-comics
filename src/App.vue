e9e1a94bd04d6fbb41787414c6e66594eb3ecebf
Fedekh <federicocet@gmail.com>
2023-04-13 13:46:15 +0200

    Add scaffolding and first import/export

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,21 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
+<script>
+import AppHeader from "./components/AppHeader.vue";
+export default {
+    components:{
+        AppHeader
+    }
+}
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <AppHeader/>
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+
+<style lang="scss">
+@use "./style/general.scss" as *;
+
 </style>
+
+
+<!--? In SCRIPT importo qualsiasi componente Vue che scriverò poi lo esporto nella key specifica "components", usando lo stesso name di riferimento (good practies). Nel TEMPLATE, che è da considerare come il body del nostro progetto finale, immettero tramite self-closing pseudotag. con lo stesso nomeche è in script. Nello STYLE importo lo style general, e se serve gli stili singoli se dovessi scrivere altro css al di fuori di quello general -->
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,20 @@
+<script>
+export default {
+    name: "AppHeader",
+    data(){
+        return{
+
+        }
+    }
+} 
+</script>
+
+<template>
+  <h1>MAIN HEADER</h1>
+</template>
+
+
+<style lang="scss">
+
+
+</style>
