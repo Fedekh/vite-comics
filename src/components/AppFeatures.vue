f6f41ae844111f03a22f6fd308cc25d768820362
Fedekh <federicocet@gmail.com>
2023-04-14 12:31:06 +0200

    complete AppFeatures dinamically

--- src/components/AppFeatures.vue
+++ src/components/AppFeatures.vue
@@ -2,6 +2,35 @@
 export default {
     name: "AppFeatures",
     data() {
+        return {
+            shopLinks: [
+                {
+                    img: "buy-comics-digital-comics.png",
+                    title: "digital comics",
+                    url: "#",
+                },
+                {
+                    img: "buy-comics-merchandise.png",
+                    title: "dc merchandise",
+                    url: "#",
+                },
+                {
+                    img: "buy-comics-subscriptions.png",
+                    title: "subscription",
+                    url: "#",
+                },
+                {
+                    img: "buy-comics-shop-locator.png",
+                    title: "comic shop locator",
+                    url: "#",
+                },
+                {
+                    img: "buy-dc-power-visa.svg",
+                    title: "dc power visa",
+                    url: "#",
+                },
+            ]
+        }
     },
     methods: {
         getImgPath(name) {
@@ -12,16 +41,51 @@ export default {
 </script>
 
 <template>
-   <div class="features"></div>
+    <div class="features">
+        <ul class="ancor-link">
+            <li v-for="element in shopLinks">
+                <a :href="element.url">
+                    <img :src="getImgPath(element)" alt="">
+                    <h3>{{ element.title }}</h3>
+                </a>
+            </li>
+        </ul>
+    </div>
 </template>
 
 
 <style scoped lang="scss">
 @use "../style/partials/mixins" as *;
 @use "../style/partials/variables" as *;
-.features{
+
+.features {
     width: 100%;
     height: 150px;
     background-color: $mainblu;
+
+    .ancor-link {
+        @include flex(row, space-around, center);
+        @include ms_container(100%, 0 auto);
+
+        li {
+            width: 50px;
+            font-size: .4rem;
+            color: white;
+
+            &:nth-child(4) {
+                width: 30px;
+            }
+                a {
+                    @include flex(column, center, center);
+                    gap: 10px;
+
+
+                    h3 {
+                        font-size: 1rem;
+                        text-align: center;
+                    }
+                }
+            }
+        }
     }
 </style>
